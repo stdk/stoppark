e47617d97f2b10d1b66266b2d51e7eebb9213910
feanor <std.feanor@gmail.com>
2012-06-18 17:08:19 +0300

    Major update of mid.py.
    + Implemented new method of handling communication with console
      regarding encodings. Now, any query we got will automatically
      be decoded from cp1251 to utf8, handled by sqlite and then
      encoded back to cp1251 and returned to console. This way,
      we'll greatly simplify communication in both directions,
      discarding old `enc` and `dec` functions.

--- mid.py
+++ mid.py
@@ -1,76 +1,40 @@
-#!/bin/python
-from structure import *
 from commands import getoutput
-import asyncore
-import socket
-from httplib import HTTPConnection
-from urllib import urlopen,urlencode
-import sqlite3
+from asyncore import dispatcher,dispatcher_with_send,loop
+from socket import AF_INET,SOCK_STREAM
+from sqlite3 import connect as sqlite3_connect
 
-def enc(s):
- try:
-  return s.encode('cp1251')
- except Exception as e:
-  print e
-  return 'None'
+ADDR = getoutput("/sbin/ifconfig").split("\n")[1].split()[1][5:] , 101
+connection = sqlite3_connect('data/db.db3')
+connection.text_factory = str
+cursor = connection.cursor()
 
-def dec(s):
- try:
-  return s.decode('cp1251')
- except Exception as e:
-  print e
-  return 'None'
-
-conn = sqlite3.connect('data/db.db3')
-conn.create_function('enc',1,enc)
-conn.create_function('dec',1,dec)
-conn.text_factory = str
-
-HOST = getoutput("/sbin/ifconfig").split("\n")[1].split()[1][5:]
-PORT = 101
-BASE_PORT = 2000
-
-class TcpServer(asyncore.dispatcher):
+class TcpServer(dispatcher):
  def __init__(self, (host, port), handler):
-  asyncore.dispatcher.__init__(self)
+  dispatcher.__init__(self)
   self.handler = handler
-
-  self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+  self.create_socket(AF_INET, SOCK_STREAM)
   self.set_reuse_addr()
   self.bind((host, port))
-  self.listen(5)
+  self.listen(20)
   print 'Listen on %s:%s' % (host,port)
 
  def handle_accept(self):
   pair = self.accept()
-  if pair is None:
-   print 'handle_accept: None'
-   return
-  else:
-   sock, addr = pair
-   print 'Incoming connection from %s' % repr(addr)
-   handler = self.handler(sock)
+  if pair: self.handler(pair[0])
 
-class SQLHandler(asyncore.dispatcher_with_send):
+class SQLHandler(dispatcher_with_send):
  def handle_read(self):
-  data = self.recv(1024)
+  data = self.recv(512)
   if data:
-   print data
    try:
-    c = conn.cursor()
-    c.execute(data)
-    counter = 0
-    buffer = ''
-    for row in c:
-     counter += 1
-     buffer += '|'.join( str(field) for field in row )+'\n'
-    if not counter: self.send('NONE\n')    
-    else: self.send(buffer)
-    conn.commit()
-   except Exception as e:
-    print e
-    self.send('FAIL\n') 
+    cursor.execute(data.decode('cp1251'))
+    answer = '\n'.join( '|'.join( str(field) for field in row ) for row in cursor )
+    self.send(unicode(answer).encode('cp1251')) if answer else self.send('NONE')
+    connection.commit()
+   except Exception as ex:
+    print ex
+    self.send('FAIL') 
   self.close()
 
-sql = TcpServer( (HOST,PORT),SQLHandler)
-asyncore.loop()
+sql = TcpServer( ADDR, SQLHandler)
+loop()
