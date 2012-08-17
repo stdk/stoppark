801d92366598dd088349024418d001c28dbc2bbf
feanor <std.feanor@gmail.com>
2012-08-17 12:10:24 +0300

    Implemented logging for mid.py with link in web-interface.

--- mid.py
+++ mid.py
@@ -2,6 +2,29 @@ from commands import getoutput
 from asyncore import dispatcher,dispatcher_with_send,loop
 from socket import AF_INET,SOCK_STREAM
 from sqlite3 import connect as sqlite3_connect
+import logging
+import logging.config
+
+logging.config.dictConfig({
+ 'version': 1,    # Configuration schema in use; must be 1 for now
+ 'formatters': {
+     'standard': {
+         'format': ('%(asctime)s %(name)-15s '
+                    '%(levelname)-8s %(message)s')}},
+
+ 'handlers': {'mid': { 'backupCount': 10,
+                       'class': 'logging.handlers.RotatingFileHandler',
+                       'filename': 'data/mid.log',
+                       'formatter': 'standard',
+                       'level': 'DEBUG',
+                       'maxBytes': 1000000 }
+             },
+ # Specify properties of the root logger
+ 'root': {
+          'level': 'DEBUG',
+          'handlers': ['mid']
+ },
+})
 
 ADDR = getoutput("/sbin/ifconfig").split("\n")[1].split()[1][5:] , 101
 connection = sqlite3_connect('data/db.db3')
@@ -20,19 +43,24 @@ class TcpServer(dispatcher):
 
  def handle_accept(self):
   pair = self.accept()
-  if pair: self.handler(pair[0])
+  if pair: self.handler(pair)
 
 class SQLHandler(dispatcher_with_send):
+ def __init__(self,(sock,(host,port))):
+  dispatcher_with_send.__init__(self,sock)
+  self.host = host
  def handle_read(self):
   data = self.recv(512)
   if data:
    try:
-    cursor.execute(data.decode('cp1251'))
+    query = data.decode('cp1251').strip()
+    logging.debug('Query[%s]',query)
+    cursor.execute(query)
     answer = '\n'.join( '|'.join( str(field) for field in row ) for row in cursor )
     self.send(answer.decode('utf-8').encode('cp1251')) if answer else self.send('NONE')
-    connection.commit()
+    connection.commit()    
    except Exception as ex:
-    print ex
+    logging.error('Exception[%s][%s]',ex.__class__.__name__,ex)
     self.send('FAIL') 
   self.close()
 
